e47cfa7870861243089bcf568a513cf04bfcc172
Richard LaBrecque <RichardlaBreque@Gmail.com>
2021-11-12 19:46:51 -0500

    Complete program specfic challenge

---
+++ variables.rb
@@ -0,0 +1,9 @@
+anmial = "dog"
+is_cuddly = true
+anmial = "cat"
+num_8 = 8
+num_3 = 3
+quotient = num_3 / num_8
+favorite_films = ["Batman and Robin", "Back to the future", "Thor: Ragnarok"]
+favorite_films.shift
+print animal.lenght
