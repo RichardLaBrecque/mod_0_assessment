4e91c332131139b74559af70a96fb804e2fddf37
Richard LaBrecque <RichardlaBreque@Gmail.com>
2021-11-12 19:59:12 -0500

    add house hash

---
+++ models/model_b.rb
@@ -0,0 +1,1 @@
+fake_house = {house_num: 1, num_floors: 2, color: "white", street: "main st", has_water: true, has_sewer: true, features: ["pool", "fence", "heated Driveway"], appliances: ["fridge", "stove", "toilet"]}
