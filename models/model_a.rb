9284d95af1a923a0bc4d0f38adb2b8c9fce40eda
Richard LaBrecque <RichardlaBreque@Gmail.com>
2021-11-12 20:00:38 -0500

    add second array

--- models/model_a.rb
+++ models/model_a.rb
@@ -1,1 +1,1 @@
-laptop = {brand: "Apple", model: "macbook pro", screen_inch: 13, harddrive: 245, has_usb_c: true, has_cd: false, features:["13inch screen", "touchpad", "wifi"]}
+laptop = {brand: "Apple", model: "macbook pro", screen_inch: 13, harddrive: 245, has_usb_c: true, has_cd: false, features:["13inch screen", "touchpad", "wifi"], uses: ["school", "office", "gaming"]}
