4b86ad2d8b9abc1d0a44a7ab29d4f6fa2cac556e
Richard LaBrecque <RichardlaBreque@Gmail.com>
2021-11-15 17:37:05 -0500

    correct variable.rb

--- variables.rb
+++ variables.rb
@@ -1,9 +1,17 @@
-anmial = "dog"
+animal = "dog"
+
 is_cuddly = true
-anmial = "cat"
+
+animal = "cat"
+
 num_8 = 8
+
 num_3 = 3
+
 quotient = num_3 / num_8
+
 favorite_films = ["Batman and Robin", "Back to the future", "Thor: Ragnarok"]
+
 favorite_films.shift
-print animal.lenght
+
+puts animal.length 
