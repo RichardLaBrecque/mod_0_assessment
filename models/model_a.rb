022f5cd65c362dcc9c4d14a50760198e0b39dfca
Richard LaBrecque <RichardlaBreque@Gmail.com>
2021-11-15 17:28:08 -0500

    update model files to ruby convention

--- models/model_a.rb
+++ models/model_a.rb
@@ -1,1 +1,9 @@
-laptop = {brand: "Apple", model: "macbook pro", screen_inch: 13, harddrive: 245, has_usb_c: true, has_cd: false, features:["13inch screen", "touchpad", "wifi"], uses: ["school", "office", "gaming"]}
+laptop = {
+  brand: "Apple",
+  model: "macbook pro",
+  screen_inch: 13,
+  harddrive: 245,
+  has_usb_c: true,
+  has_cd: false,
+  features:["13inch screen", "touchpad", "wifi"], uses: ["school", "office", "gaming"]
+}
--- models/model_b.rb
+++ models/model_b.rb
@@ -1,1 +1,10 @@
-fake_house = {house_num: 1, num_floors: 2, color: "white", street: "main st", has_water: true, has_sewer: true, features: ["pool", "fence", "heated Driveway"], appliances: ["fridge", "stove", "toilet"]}
+fake_house = {
+  house_num: 1,
+  num_floors: 2,
+  color: "white",
+  street: "main st",
+  has_water: true,
+  has_sewer: true,
+  features: ["pool", "fence", "heated Driveway"],
+  appliances: ["fridge", "stove", "toilet"]
+}
